028760253d3cbccddb13bcd7597137873fbb5e3d
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-11-25 13:05:15 -0200

    gerando tabelas database users e products

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $tabel->string('img_profile', 300);
+            $table->integer('active');
             $table->rememberToken();
             $table->timestamps();
+            
         });
     }
 
